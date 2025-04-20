a8afcee5d6e953983d64c8596914bc0a872d2a71
mshkrizwan23 <62351203+mshkrizwan23@users.noreply.github.com>
2025-04-20 19:57:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
-# Streamlit app placeholder
-print('Hello from app.py')
+import streamlit as st
+st.set_page_config(layout="wide")
+st.markdown("<h1 style='color:#D7263D;'>🚆 Freight Wagon Wheelset Tracker</h1>", unsafe_allow_html=True)
